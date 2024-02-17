599a79c1a4ac62c41056126ef0cf704791133327
dam.samnang <kcattor@gmail.com>
2024-02-17 12:17:52 -0800

    SEO

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]/route';
 import Image from 'next/image';
 import coffee from '@/public/images/caffe.jpg';
+import { Metadata } from 'next';
+import { prisma } from '@/prisma/client';
 
 export default async function Home() {
   // const session = await getServerSession(authOptions);
@@ -23,4 +25,21 @@ export default async function Home() {
       />
     </main>
   );
+}
+
+// export const metadata: Metadata = {
+//   title: 'Home Again',
+// };
+
+export async function generateMetadata(): Promise<Metadata> {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: 'kcattor@gmail.com',
+    },
+  });
+
+  return {
+    title: user?.email,
+    description: 'user.description',
+  };
 }
